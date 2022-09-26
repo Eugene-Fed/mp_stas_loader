3063c1cba0cce807ee2840a1dfe0b9cf0293eb24
Eugene-Fed <eugene.a.fed@gmail.com>
2022-09-26 15:21:53 +0300

    fix: file_handler.py - вынес сохранение файла в отдельный метод save_file

---
+++ request_handler.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+import json
+
+
+def get_data(url: str, headers: json) -> json:
+    """
+    Отправляем запрос
+    :param url: String
+    :param headers: json
+    :return: json
+    """
+    pass
+
+
+def post_data(url: str, headers: json) -> None:
+    """
+
+    :param url:
+    :param headers:
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
+else:
+    pass
